14cc6fba7c9c161019bd3edc39da84be700915c2
Claudia <claudiaareneee@gmail.com>
2020-06-03 19:35:50 -0500

    added images yay

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
+import test from './test.svg';
+import deck from './STANDARD--COLOR_52_FACES.svg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={test} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
