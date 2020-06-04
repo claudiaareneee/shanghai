9e824b62db6a07e281ce354252df7c20d6dee2a1
Claudia <claudiaareneee@gmail.com>
2020-06-03 19:50:14 -0500

    adding card component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import test from './test.svg';
-import deck from './STANDARD--COLOR_52_FACES.svg'
+import Card from './Card';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={test} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card/>
     </div>
   );
 }
