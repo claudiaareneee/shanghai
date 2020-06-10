fd434d022b5d6119d1bc6f992bcfb160af7f2553
Claudia <claudiaareneee@gmail.com>
2020-06-10 14:46:04 -0500

    added PlayingCard.css

--- src/components/PlayingCard.js
+++ src/components/PlayingCard.js
@@ -1,43 +1,35 @@
 import React from "react";
 import { Col } from "react-bootstrap";
 import CardSources from "./CardSources";
+import "../styles/PlayingCard.css";
 
 function PlayingCard(props) {
   const absOffset = Math.abs(props.offset);
   const yTranslation = absOffset * absOffset * (1 / 10.0);
   const rotation = props.offset * 2;
 
-  const fanStyle = {
+  const setUpStyle = {
     WebkitTransition: "all", // note the capital 'W' here
     msTransition: "all", // 'ms' is the only lowercase vendor prefix
-    transform: `rotate(${rotation}deg) translateY(${yTranslation}rem)`,
-    marginBottom: `${yTranslation}rem`,
   };
 
-  const slideStyle = {};
-
-  const pinwheelStyle = {
-    WebkitTransition: "all", // note the capital 'W' here
-    msTransition: "all", // 'ms' is the only lowercase vendor prefix
-    transform: `rotate(${10 * rotation}deg)`,
+  const styles = {
+    fanStyle: {
+      transform: `rotate(${rotation}deg) translateY(${yTranslation}rem)`,
+      marginBottom: `${yTranslation}rem`,
+    },
+    slideStyle: {},
+    pinwheelStyle: { transform: `rotate(${10 * rotation}deg)` },
+    discardStyle: {
+      transform: `rotate(${10 * Math.floor(Math.random() * 36)}deg)`,
+    },
   };
-
-  const discardStyle = {
-    WebkitTransition: "all", // note the capital 'W' here
-    msTransition: "all", // 'ms' is the only lowercase vendor prefix
-    transform: `rotate(${10 * Math.floor(Math.random() * 36)}deg)`,
-  };
-
-  const style =
-    props.useStyle === "fanStyle"
-      ? fanStyle
-      : props.useStyle === "slideStyle"
-      ? slideStyle
-      : discardStyle;
 
   return (
-    // <Col className='PlayingCard' style={props.useStyle ? fanStyle : slideStyle }>
-    <Col className="PlayingCard" style={style}>
+    <Col
+      className="PlayingCard"
+      style={{ ...setUpStyle, ...styles[props.useStyle] }}
+    >
       <img
         className="CardImage"
         src={CardSources.fronts[props.id % 54]}
