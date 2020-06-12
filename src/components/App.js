53cd8152f44b20b352478231fabfaa32523002dd
Claudia <claudiaareneee@gmail.com>
2020-06-12 15:08:52 -0500

    attempted main start page

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import GamePage from "./gamePage/GamePage";
+import StartPage from "./startPage/StartPage";
 
 function App(props) {
   return (
     <>
-      <GamePage />
+      <StartPage />
     </>
   );
 }
--- src/components/startPage/StartPage.js
+++ src/components/startPage/StartPage.js
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import "./StartPage.css";
+import GameForm from "./GameForm";
+import StartSelection from "./StartSelection";
+import { Jumbotron } from "react-bootstrap";
 
 function StartPage(props) {
-  return <div>Welcome</div>;
+  const [errors, setErrors] = useState({});
+  const [game, setGame] = useState({
+    name: "",
+    room: "",
+    selection: "",
+  });
+
+  function handleChange({ target }) {
+    setGame({
+      ...game,
+      [target.name]: target.value,
+    });
+  }
+
+  function handleSelection({ target }) {
+    setGame({
+      ...game,
+      selection: target.value,
+    });
+  }
+
+  function formIsValid() {
+    const _errors = {};
+
+    if (!game.name) _errors.name = "Name is required";
+    if (game.selection === "join" && !game.room)
+      _errors.room = "Room is required";
+
+    setErrors(_errors);
+
+    return Object.keys(_errors).length === 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (!formIsValid()) return;
+  }
+
+  return (
+    <div className="">
+      <Jumbotron className="Header">
+        <h1>Shanghai</h1>
+      </Jumbotron>
+      <div className="d-flex justify-content-center">
+        <div className="GameSelection">
+          <StartSelection onSelection={handleSelection} />
+          <GameForm
+            errors={errors}
+            game={game}
+            onChange={handleChange}
+            onSubmit={handleSubmit}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default StartPage;
