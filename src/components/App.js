bd02ab88d62dc7bc6b6056c2ba6f181cf08063b8
Claudia <claudiaareneee@gmail.com>
2020-06-12 13:33:34 -0500

    moved the game to it's own page

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { getGameById } from "../api/gameApi";
-import { getPlayerById } from "../api/playerApi";
-import "./App.css";
-import CardTable from "./gamePage/CardTable";
-import Sidebar from "./gamePage/Sidebar";
-import { Row, Col } from "react-bootstrap";
+import React from "react";
+import GamePage from "./gamePage/GamePage";
 
 function App(props) {
-  const [game, setGame] = useState({
-    discard: [],
-    draw: [],
-    players: [],
-  });
-
-  const [player, setPlayer] = useState({ cards: [] });
-
-  useEffect(() => {
-    getGameById(23421).then((_game) => {
-      setGame(_game);
-    });
-  }, []);
-
-  useEffect(() => {
-    getPlayerById(43521).then((_player) => setPlayer(_player));
-  }, []);
-
   return (
-    <div className="App">
-      <Row style={{ width: "100%" }}>
-        <Col>
-          <CardTable game={game} player={player} />
-        </Col>
-        <Col className="SidebarCol" xs lg="5">
-          <Sidebar cardsLaid={game.players} />
-        </Col>
-      </Row>
-    </div>
+    <>
+      <GamePage />
+    </>
   );
 }
 
