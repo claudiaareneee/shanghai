f37374c359b60132fc2bb6d8005b984cc92e856b
Claudia Nelson <claudiaareneee@gmail.com>
2020-06-19 17:54:43 -0500

    well here goes nothing

--- src/components/common/Constants.js
+++
@@ -1,7 +0,0 @@
-export const CARD_HEIGHT = 10;
-export const CARD_WIDTH = 7.16;
-export const CARDSTACK_STRETCH_X = 0.1;
-export const CARDSTACK_STRETCH_Y = 0.05;
-export const CARDSET_STRETCH_X = 0;
-export const CARDSET_STRETCH_Y = 0.01;
-export const CARDSLIDE_STRETCH_X = 1;
--- src/redux/initialState.js
+++
@@ -1,4 +0,0 @@
-export default {
-  courses: [],
-  apiCallsInProgress: 0,
-};
