76db930c20410f234bde3861106bb2e6ba2575fe
Claudia Nelson <claudiaareneee@gmail.com>
2020-06-19 21:13:04 -0500

    added some routing

---
+++ server/routes/players.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const data = require("../tools/mockData");
+
+// middleware that is specific to this router
+router.use(function timeLog(req, res, next) {
+  console.log("Time: ", Date.now());
+  next();
+});
+// define the home page route
+router.get("/", function (req, res) {
+  res.json(data.players);
+});
+// define the about route
+router.get("/:playerId", function (req, res) {
+  res.json(data.players[req.params.playerId]);
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const players = require("./routes/players");
+const games = require("./routes/games");
 
 const app = express();
 
@@ -21,13 +23,17 @@ app.get("/api/passwords", (req, res) => {
   console.log(`Sent ${count} passwords`);
 });
 
+app.use("/api/players", players);
+
+app.use("/api/games", games);
+
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 app.listen(port);
 
 console.log(`Password generator listening on ${port}`);
