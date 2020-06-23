eb92eb2fe4607a04965c3f451ea659575dd9e6c7
Claudia Nelson <claudiaareneee@gmail.com>
2020-06-23 16:22:40 -0500

    removed opponents in startpage

--- client/src/components/startPage/StartPage.js
+++ client/src/components/startPage/StartPage.js
@@ -51,8 +51,6 @@ function StartPage({ history }) {
 
     const _player = await playerApi.createPlayer(_game.id, { name: form.name });
     localStorage.setItem("uid", _player.id);
-
-    gameApi.addPlayerToGame(_game, _player.id);
   }
 
   async function handleSubmit(event) {
