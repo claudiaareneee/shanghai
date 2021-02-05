4ed9a3c561dcb865ae8d9260043bdddac7425d85
Claudia <claudiaareneee@gmail.com>
2021-02-04 22:24:55 -0600

    inital loading icon on waiting page

--- src/components/waitingRoomPage/PlayerList.js
+++ src/components/waitingRoomPage/PlayerList.js
@@ -3,6 +3,7 @@ import { ListGroup } from "react-bootstrap";
 import "./PlayerList.css";
 import PropTypes from "prop-types";
 import { Button, Col, Row } from "react-bootstrap";
+import { Hearts } from "@agney/react-loading";
 
 function PlayerList({ players, onClick, gameId, numberOfDecks, onChange }) {
   return (
@@ -18,7 +19,18 @@ function PlayerList({ players, onClick, gameId, numberOfDecks, onChange }) {
               </ListGroup.Item>
             ))}
           </ListGroup>
-          <p>Waiting for others to join...</p>
+          <p
+            className="d-flex justify-content-end"
+            style={{
+              display: "inline-flex",
+              width: "100%",
+              justifyContent: "right",
+            }}
+          >
+            Waiting for others to join
+            <Hearts width="40" style={{ paddingLeft: "4px" }} />
+          </p>
+
           <form onSubmit={onClick}>
             <div className="form-group row">
               <label htmlFor="numberOfDecks" className="col-sm">
